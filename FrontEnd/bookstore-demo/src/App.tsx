c1faad49b4cd7192809a796236c9a0e0099a5e9d
dopayt <deepshah4994@gmail.com>
2022-07-08 19:42:38 +0530

    second commit

---
+++ FrontEnd/bookstore-demo/src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { ThemeProvider } from "@material-ui/core/styles";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import "./assets/css/style.css";
+import { theme } from "./utils/theme";
+import MainNavigation from "./components/MainNavigation";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import { AuthWrapper } from "./context/auth";
+import { CartWrapper } from './context/cart';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <React.Suspense fallback={<></>}>
+        <BrowserRouter>
+          <AuthWrapper>
+            <CartWrapper>
+          <div className="wrapper">
+          <Header />
+            <main>
+              <MainNavigation />
+            </main>
+            <Footer />
+          </div>
+          </CartWrapper>
+          <ToastContainer />
+          
+          </AuthWrapper>
+        </BrowserRouter>
+      </React.Suspense>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ FrontEnd/bookstore-demo/src/models/RoleModel.ts
@@ -0,0 +1,7 @@
+import { number } from "yup/lib/locale"
+import UserModel from "./UserModel";
+export default class RoleModel {
+  id!: number;
+  name!: string;
+ // user!:UserModel;
+}
